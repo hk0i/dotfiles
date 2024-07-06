4eb2cb65ed4cc83e93e75b9f4edab67d792ffd02
Gregory <755669+hk0i@users.noreply.github.com>
2024-07-05 20:42:32 -0700

    [wezterm] add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require('wezterm')
+local config = wezterm.config_builder()
+
+-- config.color_scheme = 'Ayu Mirage'
+-- config.color_scheme = 'Tango (base16)'
+config.color_scheme = 'Galaxy'
+config.font = wezterm.font 'Intel One Mono'
+config.font_size = 15
+
+return config
