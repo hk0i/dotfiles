da967342d52ddd389d1f5c23413b04935213113a
Gregory <755669+hk0i@users.noreply.github.com>
2024-07-13 13:10:14 -0700

    [wezterm] use `kanagawa` theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,8 @@ local config = wezterm.config_builder()
 
 -- config.color_scheme = 'Ayu Mirage'
 -- config.color_scheme = 'Tango (base16)'
-config.color_scheme = 'Galaxy'
+-- config.color_scheme = 'Galaxy'
+config.color_scheme = 'Kanagawa'
 config.font = wezterm.font 'Intel One Mono'
 config.font_size = 15
 
