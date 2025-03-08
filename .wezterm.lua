cc68da55eb50c04b4aeabf19de38d1c91af2eea7
Pekk <755669+hk0i@users.noreply.github.com>
2025-03-08 08:52:14 -0800

    [wezterm] configure font ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,17 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'Tango (base16)'
 -- config.color_scheme = 'Galaxy'
 config.color_scheme = 'Kanagawa'
-config.font = wezterm.font 'Intel One Mono'
+-- testing www -> >= <= !=
+config.font = wezterm.font {
+    family = 'Intel One Mono',
+    harfbuzz_features = { 'ss01', 'ss02' }
+}
+-- config.harfbuzz_features = { 'ss01', 'ss02' }
+-- config.font = wezterm.font {
+--     family = 'JetBrains Mono',
+--     weight = 'Medium',
+--     harfbuzz_features = { 'calt' },
+-- }
 config.font_size = 15
 config.window_background_opacity = 0.8
 
