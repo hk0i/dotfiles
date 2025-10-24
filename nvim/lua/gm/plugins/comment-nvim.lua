82dee84380370dec28c5bb46f69c5a3543ec958b
Gregory <755669+hk0i@users.noreply.github.com>
2025-10-23 18:41:04 -0700

    [nvim][comment] enhance: add `<Leader>/` as shortcut to visual and normal commenting

--- nvim/lua/gm/plugins/comment-nvim.lua
+++ nvim/lua/gm/plugins/comment-nvim.lua
@@ -2,5 +2,11 @@ return {
     'numToStr/Comment.nvim',
     opts = {
         -- add any options here
+        toggler = {
+            line = '<Leader>/'
+        },
+        opleader = {
+            line = '<Leader>/'
+        }
     }
 }
