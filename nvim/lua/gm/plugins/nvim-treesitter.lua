870061ab6eef2f856a054b4d0f925af0cac7da23
pekk <755669+hk0i@users.noreply.github.com>
2026-03-31 16:10:48 -0700

    [lazy][tree sitter] fix: broken tree sitter config from version upgrade; update lock file

--- nvim/lua/gm/plugins/nvim-treesitter.lua
+++ nvim/lua/gm/plugins/nvim-treesitter.lua
@@ -1,34 +1,32 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  -- Optional: add build = ":TSUpdate" to ensure parsers update with the plugin
+  build = ":TSUpdate",
   config = function()
-
-    require('nvim-treesitter.configs').setup {
-      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    -- CHANGE: We now require 'nvim-treesitter' directly instead of '.configs'
+    require('nvim-treesitter').setup({
+      -- A list of parser names, or "all"
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "swift" },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
 
       -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
 
-      -- List of parsers to ignore installing (or "all")
+      -- List of parsers to ignore installing
       ignore_install = { "javascript" },
-
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
       highlight = {
         enable = true,
 
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        disable = { "c", "rust" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- Using a function to disable for large files
         disable = function(lang, buf)
+          -- Also disabling for 'c' and 'rust' as per your original list
+          if lang == "c" or lang == "rust" then
+              return true
+          end
+
           local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
@@ -36,12 +34,8 @@ return {
           end
         end,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = { 'markdown' },
       },
-    }
+    })
   end
 }
